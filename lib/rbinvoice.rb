34714b7afbf87458c929199adc43b8922af6ecba
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2012-08-02 20:24:05 -0700

    Started code to read the hourly spreadsheet

--- lib/rbinvoice.rb
+++ lib/rbinvoice.rb
@@ -1,12 +1,31 @@
 require 'trollop'
-# require 'roo'
+require 'roo'
 # require 'prawn'
 
 require 'rbinvoice/options'
+require 'roo'
 
 module RbInvoice
 
   def self.write_invoice(client, filename, opts)
+  end
+
+  def self.hourly_breakdown(client, opts)
+    hours = read_all_hours(client, opts)
+  end
+
+  def self.open_worksheet(spreadsheet)
+    Spreadsheet.open(spreadsheet).worksheet(0)
+  end
+
+  def self.read_all_hours(client, opts)
+    ss = nil
+    begin
+      ss = open_worksheet(opts[:spreadsheet])
+    rescue Exception => e
+      $stderr.puts "rbinvoice: Failed to open spreadsheet #{opts[:spreadsheet]}: $!"
+      exit 1
+    end
   end
 
 end
